41b797b22ef7d876714c80b6cadc7c48101c3511
juicejeff19 <jalcalar1900@alumno.ipn.mx>
2021-08-31 03:45:27 -0500

    first commit

---
+++ Calculadora/src/calculadora/Calculadora.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author juice
+ */
+public class Calculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Cal_Interf c = new Cal_Interf();
+        c.setVisible(true);
+        c.setLocationRelativeTo(null);
+        
+    }
+    
+}
